e7286d80b250dd755445983203eea0ff112d25ab
Richard Bakker <richard@hoppinger.com>
2014-07-22 12:09:44 +0200

    Initial commit

---
+++ themes/media-politiek24-video.tpl.php
@@ -0,0 +1,3 @@
+<div class="<?php print $classes; ?> media-politiek24-<?php print $id; ?>">
+  <iframe class="media-politiek24-player" width="<?php print $width; ?>" height="<?php print $height; ?>" src="<?php print $url; ?>" frameborder="0" allowfullscreen></iframe>
+</div>
